0c26681e2b39eb8c318de15deb8d136d4dc03a9a
moazMo7sen <moaz.m@shahbandr.com>
2023-08-28 20:29:04 +0300

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,42 @@
-// aside show txt
-const aside = document.querySelector("aside"),
-  txts = document.querySelectorAll(".txt"),
-  arrow_aside = document.querySelector(".arrow-aside"),
-  toggle_aside = document.querySelector(".toggle-aside");
-
-arrow_aside.addEventListener("click", (e) => {
+var screen_width = $(window).width();
+if (screen_width > 768) {
+  $("aside").addClass("active");
+  $(".txt").toggleClass("d-inline");
+  $(".aside-link").toggleClass("swim");
+}
+$(".arrow-aside").on("click", function (e) {
   e.stopPropagation();
-  aside.classList.toggle("active");
-  aside.classList.toggle("hover");
-});
+  $("aside").toggleClass("active");
+  $(".txt").toggleClass("d-inline");
+  $(".aside-link").toggleClass("swim");
 
-aside.addEventListener("mouseenter", () => {
-  aside.classList.add("hover");
-  if (!aside.classList.contains("active")) {
-    txts.forEach((txt) => {
-      setTimeout(() => {
-        txt.style.display = "inline";
-      }, 50);
-    });
+  if ($("aside").hasClass("active")) {
+    $(".aside-icon").css("transform", "rotate(0)");
+  } else {
+    $(".aside-icon").css("transform", "rotate(180deg)");
   }
 });
 
-aside.addEventListener("mouseleave", () => {
-  aside.classList.add("hover");
-
-  if (!aside.classList.contains("active")) {
-    txts.forEach((txt) => {
-      txt.style.display = "none";
-    });
-  }
-});
-
-// aside small devices
-
-toggle_aside.addEventListener("click", (e) => {
+$(".toggle-aside").on("click", function (e) {
   e.stopPropagation();
-  aside.classList.add("active");
-  if (aside.classList.contains("active")) {
-    txts.forEach((txt) => {
-      txt.style.display = "inline";
-    });
-  }
+  $("aside").toggleClass("active");
+  $(".txt").toggleClass("d-inline");
+  $(".aside_overlay").addClass("active");
 });
 
-document.addEventListener("click", (e) => {
-  e.stopPropagation();
-  if (!e.target.classList.contains("aside")) {
-    txts.forEach((txt) => {
-      txt.style.display = "none";
-    });
-  }
-  aside.classList.remove("active");
+$(".aside_overlay").on("click", function (e) {
+  $("aside").removeClass("active");
+  $(".aside_overlay").removeClass("active");
+  $(".txt").removeClass("d-inline");
 });
+
+//   $(document).on("click", function (e) {
+//     e.stopPropagation();
+//   $("aside").removeClass("active");
+//   $(".txt").removeClass("d-inline");
+// });
 
 //select 2
-$("#drop_list_status").select2({
-  dropdownParent: $("#drop_list_status_con"),
-  language: {
-    noResults: function () {
-      return "لا توجد نتائج مطابقة.";
-    },
-  },
-});
-
-$("#drop_list_productDetails").select2({
-  dropdownParent: $("#drop_list_productDetails_con"),
-  language: {
-    noResults: function () {
-      return "لا توجد نتائج مطابقة.";
-    },
-  },
-});
-$("#drop_list_tax").select2({
-  dropdownParent: $("#drop_list_tax_con"),
-  language: {
-    noResults: function () {
-      return "لا توجد نتائج مطابقة.";
-    },
-  },
-});
-
-$("#drop_list_shippingDetails").select2({
-  dropdownParent: $("#drop_list_shipping_con"),
-  language: {
-    noResults: function () {
-      return "لا توجد نتائج مطابقة.";
-    },
-  },
-});
 $("select", document).each(function (i, ele) {
   $(this).select2({
     dropdownParent: $("#drop_list_con"),
@@ -98,9 +47,6 @@ $("select", document).each(function (i, ele) {
     },
   });
 });
-
-$(".drop_list_Recommendedproducts").select2({});
-$(".drop_list_marketingProducts").select2({});
 
 // slider
 $("#slider-range-min").slider({
@@ -151,9 +97,10 @@ new DataTable("#dataTable", {
 // drop_list
 
 $(".btn_open").each(function (i, ele) {
-  $(ele).on("click", function () {
-    $(this).toggleClass("active");
+  $(ele).on("click", function (e) {
+    e.stopPropagation();
 
+    $(this).toggleClass("active");
     let id = $(this).attr("id");
     let curent_ele = id.substring(0, id.lastIndexOf("_"));
 
@@ -164,5 +111,151 @@ $(".btn_open").each(function (i, ele) {
       $("#" + curent_ele).removeClass("anima");
       $("#" + curent_ele).addClass("hide");
     }
+  });
+});
+$(document).on("click", function (event) {
+  $("#drop_list_btn").removeClass("active");
+  $("#drop_list").addClass("hide");
+});
+
+//close modal
+$("#btn_modal").on("click", function () {
+  $(".modal ").modal("hide");
+});
+
+// jquery-validation
+$.validator.setDefaults({
+  ignore: ":not(select:hidden, input:visible, textarea:visible)",
+});
+
+$("#payment_acount").validate({
+  rules: {
+    payment_acount_fName: {
+      required: true,
+      minlength: 2,
+      pattern: /^[^!@#$%^&*()]*$/,
+    },
+
+    payment_acount_lName: {
+      required: true,
+      minlength: 2,
+      pattern: /^[^!@#$%^&*()]*$/,
+    },
+    payment_acount_pNanem: {
+      required: true,
+      minlength: 2,
+      pattern: /^[^!@#$%^&*()]*$/,
+    },
+
+    payment_acount_password: {
+      required: true,
+      minlength: 8,
+    },
+    payment_acount_nPassword: {
+      minlength: 8,
+    },
+    payment_acount_ncPassword: {
+      minlength: 8,
+      equalTo: '[name="payment_acount_nPassword"]',
+    },
+  },
+  messages: {
+    payment_acount_fName: {
+      minlength: jQuery.validator.format("الرجاء إدخال {2} أحرف على الأقل"),
+      required: "الرجاء إدخال اسم مستخدم",
+      pattern: "ادخل اسم مستخدم صحيح",
+    },
+    payment_acount_lName: {
+      minlength: jQuery.validator.format("الرجاء إدخال {2} أحرف على الأقل"),
+      required: "الرجاء إدخال اسم مستخدم",
+      pattern: "ادخل اسم مستخدم صحيح",
+    },
+    payment_acount_pNanem: {
+      minlength: jQuery.validator.format("الرجاء إدخال {2} أحرف على الأقل"),
+      required: "الرجاء إدخال اسم مستخدم",
+      pattern: "ادخل اسم مستخدم صحيح",
+    },
+
+    payment_acount_password: {
+      minlength: jQuery.validator.format("الرجاء إدخال {8} أحرف على الأقل"),
+      required: " الرجاء إدخال كلمة المرور",
+    },
+    payment_acount_nPassword: {
+      minlength: jQuery.validator.format("الرجاء إدخال {8} أحرف على الأقل"),
+      required: "الرجاء إدخال كلمة المرور جديدة",
+    },
+    payment_acount_ncPassword: {
+      minlength: jQuery.validator.format("الرجاء إدخال {8} أحرف على الأقل"),
+      required: "الرجاء تأكيد كلمة المرور جديدة",
+      equalTo: "كلمة المرور غير متطابقة",
+    },
+  },
+});
+$("#payment_gateway").validate({
+  ignore: [],
+  rules: {
+    card_name: {
+      required: true,
+      minlength: 2,
+      pattern: /^[^!@#$%^&*()]*$/,
+    },
+    card_number: {
+      required: true,
+      minlength: 2,
+      pattern: /^[^!@#$%^&*()]*$/,
+      number: true,
+    },
+    card_expiration: {
+      required: true,
+    },
+    card_country: {
+      required: true,
+    },
+
+    card_cvc: {
+      required: true,
+      minlength: 2,
+    },
+  },
+  messages: {
+    card_name: {
+      minlength: jQuery.validator.format("الرجاء إدخال {2} أحرف على الأقل"),
+      required: "الرجاء إدخال اسم مستخدم",
+      pattern: "ادخل اسم مستخدم صحيح",
+    },
+    card_country: {
+      required: "الرجاء إدخال الدولة",
+    },
+    card_number: {
+      minlength: jQuery.validator.format("الرجاء إدخال رقم بطاقة صحيح"),
+      required: "الرجاء إدخال رقم البطاقة",
+      pattern: "الرجاء إدخال رقم بطاقة صحيح",
+      number: "الرجاء إدخال رقم بطاقة صحيح",
+    },
+    card_expiration: {
+      required: "الرجاء إدخال تاريخ ",
+    },
+    card_cvc: {
+      minlength: jQuery.validator.format("الرجاء إدخال رقم صحيح"),
+      required: " الرجاء إدخال رقم ",
+    },
+  },
+  errorPlacement: function (error, element) {
+    if (element.attr("name") == "card_expiration") {
+      error.insertAfter(".card_expiration_error");
+    } else if (element.attr("name") == "card_country") {
+      error.insertAfter(".card_country_error");
+    } else {
+      error.insertAfter(element);
+    }
+  },
+});
+
+$(document).ready(function () {
+  $("#pickyDate").datepicker({
+    format: "mm/yyyy",
+    orientation: "bottom ",
+    container: $("#parent-container"),
+    language: "ar",
   });
 });
